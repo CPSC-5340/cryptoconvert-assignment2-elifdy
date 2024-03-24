890448c0b52d3285ad2e415cd316f076b2aff568
Elif Dede Yildirim <elifdy@auburn.edu>
2024-03-24 17:44:08 -0400

    ConverterView Models updated

--- Assignment2/CurrencyConverterViewModel.swift
+++ Assignment2/CurrencyConverterViewModel.swift
@@ -7,37 +7,6 @@
 
 import Foundation
 
-//class ConverterViewModel: ObservableObject{
-//    @Published private(set) var listOfCards = [ConvertModel]()
-//    
-//    func fetchCards() {
-//        self.listOfCards = [
-//            ConvertModel(cardContent: CurrencyItemModel(currencyName: "US Dollar", currencyCode: "USD", counrtyFlag: "🇺🇸", multiplier: 1)),
-//            ConvertModel(cardContent: CurrencyItemModel(currencyName: "Chinese Yuan", currencyCode: "CNY", counrtyFlag: "🇨🇳", multiplier: 6.94)),
-//            ConvertModel(cardContent: CurrencyItemModel(currencyName: "Canadian Dollar", currencyCode: "CAD", counrtyFlag: "🇨🇦", multiplier: 1.36)),
-//            ConvertModel(cardContent: CurrencyItemModel(currencyName: "Australian Dollar", currencyCode: "AUD", counrtyFlag: "🇦🇺", multiplier: 1.49)),
-//            ConvertModel(cardContent: CurrencyItemModel(currencyName: "Indian Rupee", currencyCode: "INR", counrtyFlag: "🇮🇳", multiplier: 82.60)),
-//            ConvertModel(cardContent: CurrencyItemModel(currencyName: "Chilean Peso", currencyCode: "CLP", counrtyFlag: "🇨🇱", multiplier: 832.04))
-//            
-//        ]
-//    }
-//    
-//    func flip(card: ConvertModel){
-//        if let index = findIndex(card: card){
-//            listOfCards[index].isFront.toggle()
-//        }
-//        
-//    }
-//    func findIndex(card: ConvertModel) -> Int?{
-//        for index in 0..<listOfCards.count{
-//            if card.id == listOfCards[index].id{
-//                return index
-//            }
-//        }
-//        return nil
-//    }
-//    
-//}
 class CurrencyConverterViewModel: ObservableObject{
     @Published private(set) var listOfCards = [ConvertModel<CurrencyItemModel>]()
     func fetchCards() {
